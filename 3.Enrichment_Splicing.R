c2127f2ccb79f0a3195f8b92f69dd773276971b6
Gerardts9 <gerardts9@gmail.com>
2023-05-25 14:49:31 +0200

    Add comparison with tunica-specific results

--- 3.Enrichment_Splicing.R
+++ 3.Enrichment_Splicing.R
@@ -1,6 +1,5 @@
 library(clusterProfiler);library(org.Hs.eg.db);library(enrichplot);library(ggplot2);library(stringr)
 
-
 res <- read.table("C://Users/Gerard/Desktop/AAA/RNAseq/SUPPA/AAA_Sig_Results.txt")
 
 event <- sub(".*;(..).*","\\1", rownames(res))
@@ -8,7 +7,7 @@ event <- sub(".*;(..).*","\\1", rownames(res))
 ggplot(data.frame(event), aes(x=event, fill = event)) +
   geom_bar()
 
-ggsave("C://Users/Gerard/Desktop/AAA/RNAseq/Plots/Splicing_Barplot.png")
+#ggsave("C://Users/Gerard/Desktop/AAA/RNAseq/Plots/Splicing_Barplot.png")
 
 
 gene_list <- unique(substr(rownames(res), 1, 15))
@@ -25,6 +24,36 @@ go_enrich <- enrichGO(gene          = entrez_ids$ENTREZID,
                       qvalueCutoff  = 0.2)
 
 
+gene_id_list <- lapply(go_enrich@result$geneID, function(x) strsplit(x, "/")[[1]])
+
+gene_symbols_df <- bitr(unlist(gene_id_list), fromType = "ENTREZID", toType = "SYMBOL", OrgDb = org.Hs.eg.db)
+
+gene_symbol_list <- lapply(gene_id_list, function(x) gene_symbols_df$SYMBOL[match(x, gene_symbols_df$ENTREZID)])
+names(gene_symbol_list) <- names(gene_id_list)
+
+
+med <- read.xlsx("C://Users/Gerard/Desktop/AAA/RNAseq/TunicaSpecificResults.xlsx", sheet = 1)
+adv <- read.xlsx("C://Users/Gerard/Desktop/AAA/RNAseq/TunicaSpecificResults.xlsx", sheet = 2)
+
+
+
+intersect(cc$V1, unique(unlist(gene_symbol_list)))
+
+
+med$Gene.Symbol <- gsub("-", "", med$Gene.Symbol)
+adv$Gene.Symbol <- gsub("-", "", adv$Gene.Symbol)
+
+med[med$Gene.Symbol %in% "FHL1",]
+adv[adv$Gene.Symbol %in% "FHL1",]
+
+med[med$Gene.Symbol %in% "VCL",]
+adv[adv$Gene.Symbol %in% "VCL",]
+
+med[med$Gene.Symbol %in% "CAV2",]
+adv[adv$Gene.Symbol %in% "CAV2",]
+
+
+
 # GO enrichment dot plot:
 png("C://Users/Gerard/Desktop/GO_Enrichment_Splicing.png", width = 4*800, height = 4*600, res = 300)
 dotplot(go_enrich, showCategory = 10) + ggtitle("GO Enrichment")
@@ -33,12 +62,12 @@ dev.off()
 
 
 # KEGG enrichment analysis: No results.
-kegg_enrich <- enrichKEGG(gene         = entrez_ids$ENTREZID,
-                          organism     = "hsa",
-                          keyType      = "kegg",
-                          pAdjustMethod = "BH",
-                          pvalueCutoff = 0.05,
-                          qvalueCutoff = 0.2)
+#kegg_enrich <- enrichKEGG(gene         = entrez_ids$ENTREZID,
+#                          organism     = "hsa",
+#                          keyType      = "kegg",
+#                          pAdjustMethod = "BH",
+#                          pvalueCutoff = 0.05,
+#                          qvalueCutoff = 0.2)
 
 
 
